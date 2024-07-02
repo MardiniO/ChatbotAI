8febf1d132809ad3240bfac4cd657851bca3224a
Mardini <Mardini@Omars-MacBook-Pro.local>
2024-07-02 11:03:48 +0300

    Finished Navbar, still not functional.

--- ChatbotAI/src/Components/Navbar/Navbar.jsx
+++ ChatbotAI/src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
 import './Navbar.css'
-import "bootstrap/dist/css/bootstrap.min.css";
+import chatLogo from '../../assets/chatbot.png'
 
 const Navbar = () => {
   return (
-    <div className="container">
-        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Pricing</a>
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
+    <div className="navContainer">
+        <div className="titleContainer">
+            <img src={ chatLogo } alt="Chatbot Logo" className="logo"/>
+            <h1 className="title"><b> ChatBot </b></h1>
+        </div>
+        <ul>
+            <li> Home </li>
+            <li> Admin </li>
+            <li> About </li>
+        </ul>
     </div>
   )
 }
