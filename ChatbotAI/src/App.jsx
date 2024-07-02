4b5157db41ecce8671c526799c913dd74daceca8
Mardini <Mardini@Omars-MacBook-Pro.local>
2024-07-02 17:19:35 +0300

    More progress on the main page. Progress on the chat UI.

--- ChatbotAI/src/App.jsx
+++ ChatbotAI/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import ChatbotUI from './Components/ChatbotUI/ChatbotUI'
 
 
 const App = () => {
   return (
     <div className="container">
-      <Navbar/>
+      <div className="navContainer">
+        <Navbar/>
+      </div>
+      <div className="botContainer">
+        <ChatbotUI/>
+      </div>
     </div>
   )
 }
--- ChatbotAI/src/Components/Navbar/Navbar.jsx
+++ ChatbotAI/src/Components/Navbar/Navbar.jsx
@@ -4,17 +4,17 @@ import chatLogo from '../../assets/chatbot.png'
 
 const Navbar = () => {
   return (
-    <div className="navContainer">
-        <div className="titleContainer">
-            <img src={ chatLogo } alt="Chatbot Logo" className="logo"/>
-            <h1 className="title"><b> ChatBot </b></h1>
-        </div>
-        <ul>
-            <li> Home </li>
-            <li> Admin </li>
-            <li> About </li>
-        </ul>
+    <>
+    <div className="titleContainer">
+        <img src={ chatLogo } alt="Chatbot Logo" className="logo"/>
+        <h1 className="title"><b> ChatBot </b></h1>
     </div>
+    <ul>
+        <li> Home </li>
+        <li> Admin </li>
+        <li> About </li>
+    </ul>
+    </>
   )
 }
 
