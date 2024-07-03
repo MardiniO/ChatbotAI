21e95dacf3f2907ccc4ac58d6d28bbaab905b07e
Mardini <Mardini@Omars-MacBook-Pro.local>
2024-07-03 14:20:27 +0300

    Same commit.

---
+++ frontend2/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+import chatLogo from '../../assets/chatbot.png'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="titleContainer">
+        <img src={ chatLogo } alt="Chatbot Logo" className="logo"/>
+        <h1 className="title"><b> ChatBot </b></h1>
+    </div>
+    <ul>
+        <li> Home </li>
+        <li> Admin </li>
+        <li> About </li>
+    </ul>
+    </>
+  )
+}
+
+export default Navbar
