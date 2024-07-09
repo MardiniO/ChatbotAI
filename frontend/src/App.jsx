f39b81719ca57d38f6b0b9c95d6f88a620441223
Mardini <Mardini@Omars-MacBook-Pro.local>
2024-07-09 11:13:33 +0300

    Added Navbar functionality. Still needs CSS modifications.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import ChatbotUI from './Components/ChatbotUI/ChatbotUI'
-
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ChatbotUI from "./Pages/ChatbotUI/ChatbotUI";
+import Admin from "./Pages/Admin/Admin";
+import About from "./Pages/About/About";
 
 const App = () => {
   return (
     <div className="container">
-      <div className="navContainer">
-        <Navbar/>
-      </div>
-      <div className="botContainer">
-        <ChatbotUI/>
-      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ChatbotUI />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import './Navbar.css'
-import chatLogo from '../../assets/chatbot.png'
+import React from "react";
+import chatLogo from "../../assets/chatbot.png";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-    <div className="titleContainer">
-        <img src={ chatLogo } alt="Chatbot Logo" className="logo"/>
-        <h1 className="title"><b> ChatBot </b></h1>
-    </div>
-    <ul>
-        <li> Home </li>
-        <li> Admin </li>
-        <li> About </li>
-    </ul>
+      <Nav>
+        <Bars />
+        <NavMenu>
+          <div className="titleContainer">
+            <img src={chatLogo} alt="Chatbot Logo" className="logo" />
+            <h1 className="title">
+              <b> ChatBot </b>
+            </h1>
+          </div>
+          <NavLink to="/"> Home </NavLink>
+          <NavLink to="/admin"> Admin </NavLink>
+          <NavLink to="/about"> About </NavLink>
+        </NavMenu>
+      </Nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
