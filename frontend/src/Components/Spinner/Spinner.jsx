8820877e2ccfc811bc1920a8f9ed7c246f3e786b
Mardini <Mardini@Omars-MacBook-Pro.local>
2024-07-29 14:10:35 +0300

    Spinner still not working. Everything mostly organized. Added database files for any external use. Removed difflib functionality... using Cohere AI instead.

---
+++ frontend/src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+// Spinner.js
+import React from "react";
+import "./Spinner.css"; // You can customize the spinner styles in this file
+
+const Spinner = () => {
+  return <div className="spinner"></div>;
+};
+
+export default Spinner;
